2faec7eaf797dc58e866d7512571b8cd950b27dd
Sergey Konshu <sk.web@mail.ru>
2021-10-07 22:39:03 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -7,23 +7,23 @@ import { ConfigProvider } from 'antd';
 import { MapProvider } from "./components/Map/MapProvider";
 
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyB5nx4s-0QtrjIWJDOfHBUOzJFldLkR-6o",
-    authDomain: "hakaton-21.firebaseapp.com",
-    projectId: "hakaton-21",
-    storageBucket: "hakaton-21.appspot.com",
-    messagingSenderId: "740901348912",
-    appId: "1:740901348912:web:2b31e4d1330322e1ea6ff5"
-};
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: "AIzaSyB5nx4s-0QtrjIWJDOfHBUOzJFldLkR-6o",
+//     authDomain: "hakaton-21.firebaseapp.com",
+//     projectId: "hakaton-21",
+//     storageBucket: "hakaton-21.appspot.com",
+//     messagingSenderId: "740901348912",
+//     appId: "1:740901348912:web:2b31e4d1330322e1ea6ff5"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 ReactDOM.render(<ConfigProvider ><MapProvider>
     <App />
 </MapProvider>,</ConfigProvider>, document.getElementById('root'));
