145aca170620a4a83fe2966d1e3ecc4d4ebf1511
Sergey Konshu <sk.web@mail.ru>
2021-10-08 09:19:39 +0300

    fix enum

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ export const MAIN_URL = 'http://84.201.139.152:8080'
 export const GET_INITIAL_POINTS_ENDPOINT = '/compass/get-point-list'
 export const START_CALC_ENDPOINT = '/compass/get-tour'
 export enum TRANSPORT_TYPE {
-    CAR = 'car',
-    PEDO = 'pedo',
+    CAR = 'shortest',
+    PEDO = 'pedestrian',
     BICYCLE = 'bicycle'
 }
